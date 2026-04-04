6f2b0c33c3bdd7db3181ee8e20b0860095245bf5
SaintDio0 <Diogofurtado301@gmail.com>
2026-04-04 20:11:14 -0300

    Add files via upload

---
+++ avisos/urls.py
@@ -0,0 +1,14 @@
+﻿from django.urls import path
+
+from .views import AvisoCreateView, AvisoDeleteView, AvisoDetailView, AvisoListView, AvisoUpdateView
+
+app_name = "avisos"
+
+urlpatterns = [
+    path("", AvisoListView.as_view(), name="lista"),
+    path("novo/", AvisoCreateView.as_view(), name="novo"),
+    path("<int:pk>/", AvisoDetailView.as_view(), name="detalhe"),
+    path("<int:pk>/editar/", AvisoUpdateView.as_view(), name="editar"),
+    path("<int:pk>/excluir/", AvisoDeleteView.as_view(), name="excluir"),
+]
+
